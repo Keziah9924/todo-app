edf2537ec00d933d37beb06b4c35a6c96c10e153
keziah9924 <keziahtuffour@gmail.com>
2026-06-29 09:22:33 +0000

    Update My Task page layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import VitalTasks from "./pages/VitalTasks";
+import MyTask from "./pages/MyTask";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/vital-tasks" element={<VitalTasks />} />
+                    <Route path="/my-tasks" element={<MyTask />} />
                 </Routes>
             </div>
         </Router>
