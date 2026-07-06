389e6559d54f3ca3e1dc2259a9794d568570681c
keziah9924 <keziahtuffour@gmail.com>
2026-07-06 15:39:21 +0000

    Merge origin/develop into feat/task-categories-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import VitalTasks from "./pages/VitalTasks";
+import MyTask from "./pages/MyTask";
+import TaskCategories from "./pages/TaskCategories";
 
 const App = () => {
     return (
@@ -14,6 +16,8 @@ const App = () => {
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/vital-tasks" element={<VitalTasks />} />
+                    <Route path="/my-tasks" element={<MyTask />} />
+                    <Route path="/task-categories" element={<TaskCategories />} />
                 </Routes>
             </div>
         </Router>
