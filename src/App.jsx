6d77ddf6c9b1745bacf2286d0d3db7fca886471d
keziah9924 <keziahtuffour@gmail.com>
2026-07-06 14:46:45 +0000

    Implement Task Categories page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import VitalTasks from "./pages/VitalTasks";
+import TaskCategories from "./pages/TaskCategories";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/vital-tasks" element={<VitalTasks />} />
+                    <Route path="/task-categories" element={<TaskCategories />} />
                 </Routes>
             </div>
         </Router>
